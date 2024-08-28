aa00e9a4f06ea2e15cd3b2b111dcf33d0df40d22
Romiflorentin <160328621+Romiflorentin@users.noreply.github.com>
2024-08-28 18:54:26 -0300

    Eventos, DOM, localStora y JSON

--- js/index.js
+++ js/index.js
@@ -1,69 +1,112 @@
-/// Objetos
-class Celular {
-    constructor(marca, modelo, memoriaRam, precio) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.memoriaRam = memoriaRam;
-        this.precio = precio;
-    }
 
-    mostrarInfo() {
-        return `Marca: ${this.marca}, Modelo: ${this.modelo}, Memoria RAM: ${this.memoriaRam}, Precio: $${this.precio}`;
-    }
+const cuerpo = document.body;
+const carrito = []
 
-    aplicarDescuento(porcentaje) {
-        return this.precio - (this.precio * (porcentaje / 100));
-    }
+/// Modo oscuro
+const toggle = document.getElementById('toggle');
+const label_toggle = document.getElementById('label_toggle');
 
-    mostrarInfoConDescuento(porcentaje) {
-        const precioConDescuento = this.aplicarDescuento(porcentaje);
-        return `${this.mostrarInfo()}, Precio con Descuento: $${precioConDescuento.toFixed(2)}`;
+toggle.addEventListener('change', (event) => {
+    const checked = event.target.checked
+    document.body.classList.toggle('dark')
+    if (checked == true) {
+        label_toggle.innerHTML = '<i class="bi bi-brightness-low-fill"></i>';
+    } else {
+        label_toggle.innerHTML = '<i class="bi bi-moon-stars-fill"></i>';
     }
-    calcularPrecioConIVA() {
-        const IVA = 0.21;
-        return this.precio * (1 + IVA);
-    }
+})
+//Creo un menu de navegacion desde DOM
+document.addEventListener("DOMContentLoaded", function () {
+    const navContainer = document.getElementById('nav-container');
 
-    mostrarInfoConIVA() {
-        const precioConIVA = this.calcularPrecioConIVA();
-        return `${this.mostrarInfo()}, Precio con IVA: $${precioConIVA.toFixed(2)}`;
+    const nav = document.createElement('nav');
+    const ul = document.createElement('ul');
+
+    const menuItems = [
+        { text: 'Inicio', href: '#home' },
+        { text: 'Servicios', href: '#services' },
+        { text: 'Acerca de', href: '#about' },
+        { text: 'Contacto', href: '#contact' }
+    ];
+    menuItems.forEach(item => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.textContent = item.text;
+        a.href = item.href;
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+
+    nav.appendChild(ul);
+    navContainer.appendChild(nav);
+});
+
+
+const productos = [
+    {
+        id: 1,
+        nombre: 'Samsung A33',
+        img: '/assets/producto/A33.jpg',
+        precio: 300000,
+    },
+    {
+        id: 2,
+        nombre: 'Samsung S24',
+        img: '/assets/producto/S24.jpg',
+        precio: 250000,
+    },
+    {
+        id: 3,
+        nombre: 'Samsung A54',
+        img: '/assets/producto/A54.jpg',
+        precio: 350000,
+    },
+    {
+        id: 4,
+        nombre: 'Samsung S21',
+        img: '/assets/producto/S21.jpg',
+        precio: 500000,
+    },
+    {
+        id: 5,
+        nombre: 'Samsung Z Fold4',
+        img: '/assets/producto/Zfold4.jpg',
+        precio: 550000,
     }
+];
+
+
+function addtoCart(nombreProducto) {
+    let product = productos.find(p => p.nombre === nombreProducto);
+    productos.push({
+        nombre: product.nombre,
+        precio: product.precio,
+    });
+    localStorage.setItem('carrito', JSON.stringify(productos));   ///localStorage y JSON
 }
-const celular1 = new Celular("Samsung", "A33", "14Gb", 300000);
-const celular2 = new Celular("Samsung", "S24", "16Gb", 250000)
-const celular3 = new Celular("Samsung", "A54", "32Gb", 350000)
-const celular4 = new Celular("Samsung", "S21", "8Gb", 150000)
-const celular5 = new Celular("Samsung", "Z Flit", "14Gb", 400000)
-console.log(celular1.mostrarInfoConIVA()); /// muestra info del producto (precio mas iva)
-console.log(celular2.mostrarInfoConDescuento(10)); /// muestra info del producto (precio con descuento)
 
+function CardInicio(productos) {
+    const productosRow = document.getElementById('productosRow');
 
-/// Arrays
-const stock = [
-    new Celular("Samsung", "A33", "14Gb", 300000),
-    new Celular("Samsung", "S24", "16Gb", 250000),
-    new Celular("Samsung", "A54", "32Gb", 350000),
-    new Celular("Samsung", "S21", "8Gb", 150000),
-    new Celular("Samsung", "Z Flit", "14Gb", 400000)
-];
+    productos.forEach(producto => {
+        const nuevoProducto = document.createElement("div");
+        nuevoProducto.classList.add("col-md-4");
 
-// stock.forEach(celular => {
-//     console.log(celular.mostrarInfo());
-// });
-const otrosCelulares = [
-    new Celular("Apple", "iPhone 13", "6Gb", 800000),
-    new Celular("Xiaomi", "Mi 11", "8Gb", 500000)
-];
-const todosLosCelulares = stock.concat(otrosCelulares);
-// console.log(todosLosCelulares);
+        nuevoProducto.innerHTML = `
+            <div class="card mb-4 shadow-sm">
+                <img src="${producto.img}" class="card-img-top" alt="${producto.nombre}">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">Precio: $${producto.precio}</p>
+                    <button class="btn btn-primary" onclick="addtoCart('${producto.nombre}')">Añadir al Carrito</button>
+                </div>
+            </div>
+        `;
 
-/// Funciones
-function mostrarStock() {
-    stock.forEach(celular => {
-        console.log(celular.mostrarInfo());
+        productosRow.appendChild(nuevoProducto);
     });
 }
-mostrarStock()
 
-const mejorPrecio = stock.filter(Celular => Celular.precio < 300000)
-console.log(mejorPrecio)
+document.addEventListener('DOMContentLoaded', () => {
+    CardInicio(productos);
+});
